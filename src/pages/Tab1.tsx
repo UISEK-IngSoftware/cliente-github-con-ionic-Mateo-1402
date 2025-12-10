93fe4c8c5ab6e2dd1623332528e10a94e8b2ca97
Mateo Rodriguez <mateo.rodriguez@uisek.edu.ec>
2025-12-10 10:29:37 -0500

    Laboratorio 8 Mateo Rodríguez

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,46 +1,51 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList } from '@ionic/react';
+import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar, useIonViewDidEnter } from '@ionic/react';
+import { useState } from 'react';
 import './Tab1.css';
 import RepoItem from '../components/Repoitem';
+import { RepositoryItem } from '../interfaces/RepositoryItem';
+import { fetchRepositories } from '../services/GithubServices';
 
 const Tab1: React.FC = () => {
+  const[repos,setRepos] = useState<RepositoryItem[]> ([]); 
+
+  const loadRepos = async () => {
+
+    const reposData = await fetchRepositories(); 
+    setRepos(reposData);  
+
+  }; 
+
+  useIonViewDidEnter(() => { 
+    console.log("IonViewDidEnter - Cargando Repositorios"); 
+    loadRepos(); 
+  }); 
+
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Repositorios</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Repositorios</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonList>
-          {/* Repositorio original */}
-          <RepoItem
-            name="android-project"
-            imageurl="https://cdn.pixabay.com/photo/2018/05/08/21/08/android-3383929_640.png"
-          ></RepoItem>
+        <IonList> 
+          {repos.map(( repo, index) => (
+          <RepoItem 
+            key={index} 
+            repo={repo} 
+          
+            /> 
+          ))}
 
-          {/* Repositorio 1 añadido */}
-          <RepoItem
-            name="web-react-app"
-            imageurl="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
-          ></RepoItem>
-
-          {/* Repositorio 2 añadido */}
-          <RepoItem
-            name="backend-service-node"
-            imageurl="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1280px-Node.js_logo.svg.png"
-          ></RepoItem>
-
-          {/* Repositorio 3 añadido */}
-          <RepoItem
-            name="data-analysis-scripts"
-            imageurl="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png"
-          ></RepoItem>
         </IonList>
+
+
       </IonContent>
     </IonPage>
   );
